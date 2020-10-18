c3377e2d22f03270db547134b88820db1a5d9987
Roman <9malabar6@mail.ru>
2020-10-18 19:49:31 +0300

    first console programm

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 'use sctrict';
 
-const category = 'toys';
+const nomberOfFilm = +prompt('Сколько фильмов вы уже смотрели', '' );
 
-console.log(`https://someurl.com/${category}/5`);
+const personalMovieDB = {
+    count: nomberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+};
 
-const user = "Роман";
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его', '' ),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('На сколько оцените его', '' );
 
-alert (`Привет, ${user} `);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
