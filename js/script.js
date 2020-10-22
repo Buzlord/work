69f8f695adcbaccbeec9dbb15ae76155e0853721
Roman <9malabar6@mail.ru>
2020-10-22 23:50:23 +0300

    practic 4

--- js/script.js
+++ js/script.js
@@ -1,72 +1,68 @@
-'use sctrict';
-
-let nomberOfFilm;
-
-function start () {
-    nomberOfFilm = +prompt('Сколько фильмов вы уже смотрели', '' );
-
-    while(nomberOfFilm == '' || nomberOfFilm == null || isNaN(nomberOfFilm)){
-        nomberOfFilm = +prompt('Сколько фильмов вы уже смотрели', '' );
-    }
-}
-
-start ();
-
+'use strict';
 const personalMovieDB = {
-    count: nomberOfFilm,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privet: false
-};
-
-
-function rememberMyMovie(){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов', ''),
-              b = prompt('На сколько оцените его', '' );
-    
-        if (a != null && b != null && a !='' && b !='' && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
+    privet: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже смотрели', '' );
+        while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже смотрели', '' );
         }
-    }
-}
-
-rememberMyMovie();
-
-function detectedMyMovie(){
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB <= 30) {
-        console.log('вы класический зритель');
-    } else if (personalMovieDB.count > 30) {
-        console.log('Красава');
-    } else {
-        console.log('Заебал , делай правильно');
-    }
-    console.log(personalMovieDB);
-    
-}
-
-detectedMyMovie();
-
-function showMyDB(hidden){
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privet);
-
-function writeYourGenres(){
-    for(let i = 1; i <=3; i++){
-        personalMovieDB.genres[i-1] = prompt(`Ваш любимы жанр под номером ${i}`);
+    },
+    rememberMyMovie: function(){
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов', ''),
+                  b = prompt('На сколько оцените его', '' );
         
-    }
-}
+            if (a != null && b != null && a !='' && b !='' && a.length < 50){
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectedMyMovie: function () {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB <= 30) {
+            console.log('вы класический зритель');
+        } else if (personalMovieDB.count > 30) {
+            console.log('Красава');
+        } else {
+            console.log('Заебал , делай правильно');
+        }
+        },
+        showMyDB: function (hidden) {
+            if (!hidden) {
+                console.log(personalMovieDB);
+            }
+        
+        },
+        toggleVisibleDB: function(){
+            if(personalMovieDB.privet){
+                personalMovieDB.privet = false;
+            } else {
+                personalMovieDB.privet = true;
+            }
+        },
+        writeYourGenres: function (){
+            for(let i = 1; i <=3; i++){
+                let genre = prompt(`Ваш любимы жанр под номером ${i}`);
 
-writeYourGenres();
+                    if (genre === '' || genre == null ){
+                        console.log('вы ввели некорретные данные мразь');
+                        i--;
+                    } else {
+                        personalMovieDB.genres[i-1] = genre;
+                    }
+
+                personalMovieDB.genres.forEach((item, i) => {
+                    console.log(`Любимый жанра ${i + 1} это ${item}`)
+                });
+            }
+    }
+};
